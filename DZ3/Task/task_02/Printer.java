341677781c2468496fc79110900854687a29e9d0
ADSorokin <alexsksd@gmail.com>
2024-08-05 16:57:26 +0300

    Update Printer.java

--- DZ3/Task/task_02/Printer.java
+++ DZ3/Task/task_02/Printer.java
@@ -1,1 +1,41 @@
+import java.util.Arrays;
+import java.util.ArrayList;
 
+class Answer {
+    public static ArrayList<Integer> removeEvenNumbers(Integer[] arr) {
+        // Введите свое решение ниже
+    ArrayList<Integer> list = new ArrayList<>();
+
+        for (Integer a:arr) {
+            if (a%2!=0) {
+                list.add(a);
+            }
+        }
+
+                 System.out.println(list);
+
+
+
+    return list;
+    }
+}
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class Printer{
+    public static void main(String[] args) {
+        Integer[] arr = {};
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            arr = new Integer[]{2, 4, 6, 8};
+        }
+        else{
+            arr = Arrays.stream(args[0].split(", "))
+                    .map(Integer::parseInt)
+                    .toArray(Integer[]::new);
+        }
+
+        Answer ans = new Answer();
+        ans.removeEvenNumbers(arr);
+    }
+}
