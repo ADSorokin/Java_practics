d1254b21a62b0103f50ae4ee3721d083841ded04
ADSorokin <alexsksd@gmail.com>
2024-08-05 18:24:07 +0300

    Update Printer.java

--- DZ3/Task/task_03/Printer.java
+++ DZ3/Task/task_03/Printer.java
@@ -1,1 +1,67 @@
+/**
+ Реализуйте метод, который принимает на вход целочисленный массив arr:
+ - Создаёт список ArrayList, заполненный числами из исходого массива arr.
+ - Сортирует список по возрастанию и выводит на экран.
+ - Находит минимальное значение в списке и выводит на экран - Minimum is {число} - Находит максимальное значение в списке и выводит на экран - Maximum is {число}
+ - Находит среднее арифметическое значений списка и выводит на экран - Average is =  {число}
+ Напишите свой код в методе analyzeNumbers класса Answer. Метод analyzeNumbers принимает на вход один параметр:
 
+ Integer[] arr - массив целых чисел.
+
+ Пример.
+ Исходный массив:
+
+ 4, 2, 7, 5, 1, 3, 8, 6, 9
+
+ Результаты:
+
+ [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ Minimum is 1
+ Maximum is 9
+*/
+
+
+
+import java.util.*;
+
+class Answer {
+    public static void analyzeNumbers(Integer[] arr) {
+        int sum = 0;
+        // Введите свое решение ниже
+        List<Integer> arrayList = Arrays.asList(arr);
+        arrayList.sort(Comparator.naturalOrder());
+        int Maximum = Collections.max(arrayList);
+        int Minimum = Collections.min(arrayList);
+        for (Integer a : arrayList
+        ) {
+            sum += a;
+
+        }
+        double Average = (double) sum / (double)arrayList.size();
+        System.out.println(arrayList);
+        System.out.println("Minimum is "+Minimum);
+        System.out.println("Maximum is "+Maximum);
+        System.out.println("Average is "+Average);
+
+
+    }
+}
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class Printer {
+    public static void main(String[] args) {
+        Integer[] arr = {};
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            arr = new Integer[]{4, 2, 7, 5, 1, 3, 8, 6, 9};
+        } else {
+            arr = Arrays.stream(args[0].split(", "))
+                    .map(Integer::parseInt)
+                    .toArray(Integer[]::new);
+        }
+
+        Answer ans = new Answer();
+        ans.analyzeNumbers(arr);
+    }
+}
